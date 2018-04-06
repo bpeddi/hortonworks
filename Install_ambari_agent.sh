48d0472bdf4e20272f6bf815b62811168551fdd9
bpeddi <bala.peddi@gmail.com>
2018-04-05 19:24:34 -0500

    Update Install_ambari_agent.sh

--- Install_ambari_agent.sh
+++ Install_ambari_agent.sh
@@ -1,5 +1,5 @@
 wget http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.6.0.0/ambari.repo
-yum cp ambari.repo /etc/yum.repos.d/
+cp ambari.repo /etc/yum.repos.d/
 yum clean all
 yum makecache
 yum -y install ambari-agent.x86_64 ambari-server.x86_64
